6d85eb5ee9f7df64e62b733816638b77f49e4a58
mikelvizz <mvizcay@gmail.com>
2018-09-19 23:02:37 +0200

    Testing git sync

---
+++ csv_to_json.py
@@ -0,0 +1,12 @@
+from oligo import iber
+import datetime
+
+
+watt = iber.watthourmeter("mvizcay@gmail.com","2Dejulio")
+print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())+' '+str(watt))
+
+watt2 = iber.icpstatus("mvizcay@gmail.com","2Dejulio")
+print(watt2)
+
+watt3 = iber.contracts("mvizcay@gmail.com","2Dejulio")
+print(watt3)
